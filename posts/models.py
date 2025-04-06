e1acc1d932a31b7a49fe19b07b23eaeddedcf141
atari-monk <atari.monk1@gmail.com>
2025-04-06 16:16:22 +0200

    Add Post model with markdown support and initial migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+import markdown
+
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content_md = models.TextField()
+    content_html = models.TextField(editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+    is_published = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.content_html = markdown.markdown(self.content_md)
+        if self.is_published and not self.published_at:
+            self.published_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
